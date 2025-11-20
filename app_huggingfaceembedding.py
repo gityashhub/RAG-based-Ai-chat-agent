f83a49294477c9d21f0fac0e47de7cd987190aea
Yash Patil <159410035+gityashhub@users.noreply.github.com>
2025-11-20 11:23:42 +0530

    Fix import path for RecursiveCharacterTextSplitter

--- app_huggingfaceembedding.py
+++ app_huggingfaceembedding.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
